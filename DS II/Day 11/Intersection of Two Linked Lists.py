d1c7597809b8fab5ba8fad7fc8b82ebd65b331a0
nikonik33 <nikhil.1923ec1142@kiet.edu>
2022-08-07 20:35:13 +0530

    Create Intersection of Two Linked Lists.py

---
+++ DS II/Day 11/Intersection of Two Linked Lists.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getIntersectionNode(self, a: ListNode, b: ListNode) -> Optional[ListNode]:
+        alen = self.getLength(a)
+        blen = self.getLength(b)
+        
+        if alen < blen:
+            alen, blen = blen, alen
+            a, b = b, a
+            
+        diff = alen - blen
+        for _ in range(diff):
+            a = a.next
+            
+        while a and b:
+            if a is b:
+                return a
+            
+            a = a.next
+            b = b.next        
+        
+        return None
+        
+    def getLength(self, head):
+        count = 0
+        
+        while head:
+            count += 1
+            head = head.next
+        
+        return count
